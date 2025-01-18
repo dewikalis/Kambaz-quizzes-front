6d7b25266b74484228a5a0766c883dd6aa2d4204
aamaanda <94199569+aamaanda@users.noreply.github.com>
2025-01-18 15:38:20 -0500

    filler text

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -26,6 +26,27 @@ export default function Modules() {
                   Learn what is Web Development
                 </li>
               </ul>
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 1 - Introduction
+                </li>
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 2 - Creating Us
+                </li>
+              </ul>
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Introduction to Web Development
+                </li>
+                <li className="wd-content-item">
+                  Creating an HTTP server with Node.js
+                </li>
+                <li className="wd-content-item">
+                  Creating a React Application
+                </li>
+              </ul>
             </li>
           </ul>
         </li>
